189f0a7b9a4fe2dec278b99845b371544dfc6b01
Md. Shafi Mahmud <shafi.msm1999@gmail.com>
2025-01-15 20:14:29 +0600

    Update varying_brightness_led_blinking.ino

--- varying_brightness_led_blinking.ino
+++ varying_brightness_led_blinking.ino
@@ -1,19 +1,18 @@
-// const int ledPin = 13; // Pin connected to the LED
+const int ledPin = 13; 
 
-// void setup() {
-//   pinMode(ledPin, OUTPUT); // Set the LED pin as an output
-// }
+void setup() {
+  pinMode(ledPin, OUTPUT); 
+}
 
-// void loop() {
-//   // Increase brightness
-//   for (int brightness = 0; brightness <= 255; brightness++) {
-//     analogWrite(ledPin, brightness); // Set the brightness
-//     delay(10); // Wait for 10 milliseconds
-//   }
+void loop() {
+  // Increase brightness
+  for (int brightness = 0; brightness <= 255; brightness++) {
+    analogWrite(ledPin, brightness); 
+    delay(10); 
+  }
 
-//   // Decrease brightness
-//   for (int brightness = 255; brightness >= 0; brightness--) {
-//     analogWrite(ledPin, brightness); // Set the brightness
-//     delay(10); // Wait for 10 milliseconds
-//   }
-// }
+  // Decrease brightness
+  for (int brightness = 255; brightness >= 0; brightness--) {
+    analogWrite(ledPin, brightness); 
+    delay(10); 
+}
